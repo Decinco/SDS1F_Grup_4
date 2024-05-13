21b6338edef9660b4ab89342992c54f167d94606
Leandro Varas <leovaras2005@gmail.com>
2024-05-13 17:24:48 +0200

    Creacion de DataSets

--- CLS_Leandro_Varas/CLS_Leandro_Varas/frm_lista.Designer.cs
+++ CLS_Leandro_Varas/CLS_Leandro_Varas/frm_lista.Designer.cs
@@ -29,13 +29,28 @@ namespace CLS_Leandro_Varas
         /// </summary>
         private void InitializeComponent()
         {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.F1_Grand_Prix_Information1 = new CLS_Leandro_Varas.F1_Grand_Prix_Information();
             this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.F1_Grand_Prix_Information1;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.crystalReportViewer1.TabIndex = 0;
             // 
             // frm_lista
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.crystalReportViewer1);
             this.Name = "frm_lista";
             this.Text = "frm_lista";
             this.Load += new System.EventHandler(this.frm_lista_Load);
@@ -44,5 +59,8 @@ namespace CLS_Leandro_Varas
         }
 
         #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private F1_Grand_Prix_Information F1_Grand_Prix_Information1;
     }
 }
