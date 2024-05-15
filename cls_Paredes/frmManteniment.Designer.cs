34916c5182a5afc4348d3f7bda08146f4f4ede74
teropod24 <jx.paredes@sarria.salesians.cat>
2024-05-15 15:56:22 +0200

    Añadir DataSet en frmMantenimiento cls_Paredes

--- cls_Paredes/frmManteniment.Designer.cs
+++ cls_Paredes/frmManteniment.Designer.cs
@@ -31,7 +31,8 @@ namespace CLS_Juan_Paredes
         {
             this.components = new System.ComponentModel.Container();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.label1 = new System.Windows.Forms.Label();
+            this.f1DataSet1 = new cls_Paredes.F1DataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.f1DataSet1)).BeginInit();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
@@ -40,35 +41,28 @@ namespace CLS_Juan_Paredes
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
-            // label1
+            // f1DataSet1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(195, 148);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(240, 39);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Mantenimiento";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.f1DataSet1.DataSetName = "F1DataSet";
+            this.f1DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // frmManteniment
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1349, 582);
-            this.Controls.Add(this.label1);
             this.Name = "frmManteniment";
             this.Text = "frmMantenimiento";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.f1DataSet1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.Label label1;
+        private cls_Paredes.F1DataSet f1DataSet1;
     }
 }
 
