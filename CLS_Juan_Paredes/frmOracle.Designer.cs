0721f7fda2f5a8fd043522b0e05eb377e9295953
teropod24 <jx.paredes@sarria.salesians.cat>
2024-05-24 20:57:25 +0200

    Acabar frmOracle CLS_Juan_Paredes

--- CLS_Juan_Paredes/frmOracle.Designer.cs
+++ CLS_Juan_Paredes/frmOracle.Designer.cs
@@ -31,48 +31,53 @@ namespace CLS_Juan_Paredes
         {
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.Label iDPILOTLabel;
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.oracle_dataSet1 = new CLS_Juan_Paredes.Oracle_dataSet();
+            this.nombres_pilots = new System.Windows.Forms.ComboBox();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.oracle_dataSet1 = new CLS_Juan_Paredes.Oracle_dataSet();
             this.pILOTSTableAdapter = new CLS_Juan_Paredes.Oracle_dataSetTableAdapters.PILOTSTableAdapter();
             this.tableAdapterManager = new CLS_Juan_Paredes.Oracle_dataSetTableAdapters.TableAdapterManager();
             this.iDPILOTTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             iDPILOTLabel = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.oracle_dataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oracle_dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // iDPILOTLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(59, 139);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Pilots";
+            iDPILOTLabel.AutoSize = true;
+            iDPILOTLabel.Location = new System.Drawing.Point(303, 36);
+            iDPILOTLabel.Name = "iDPILOTLabel";
+            iDPILOTLabel.Size = new System.Drawing.Size(39, 17);
+            iDPILOTLabel.TabIndex = 3;
+            iDPILOTLabel.Text = "Pilot:";
             // 
-            // comboBox1
+            // nombres_pilots
             // 
-            this.comboBox1.DataSource = this.bindingSource1;
-            this.comboBox1.DisplayMember = "NAME";
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(119, 132);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(223, 24);
-            this.comboBox1.TabIndex = 1;
-            this.comboBox1.ValueMember = "IDPILOT";
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
-            // oracle_dataSet1
-            // 
-            this.oracle_dataSet1.DataSetName = "Oracle_dataSet";
-            this.oracle_dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            this.nombres_pilots.DataSource = this.bindingSource1;
+            this.nombres_pilots.DisplayMember = "NAME";
+            this.nombres_pilots.FormattingEnabled = true;
+            this.nombres_pilots.Location = new System.Drawing.Point(119, 70);
+            this.nombres_pilots.Name = "nombres_pilots";
+            this.nombres_pilots.Size = new System.Drawing.Size(223, 24);
+            this.nombres_pilots.TabIndex = 1;
+            this.nombres_pilots.ValueMember = "IDPILOT";
+            this.nombres_pilots.SelectedIndexChanged += new System.EventHandler(this.nombres_pilots_SelectedIndexChanged);
+            this.nombres_pilots.Click += new System.EventHandler(this.nombres_pilots_Click);
             // 
             // bindingSource1
             // 
             this.bindingSource1.DataMember = "PILOTS";
             this.bindingSource1.DataSource = this.oracle_dataSet1;
+            // 
+            // oracle_dataSet1
+            // 
+            this.oracle_dataSet1.DataSetName = "Oracle_dataSet";
+            this.oracle_dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // pILOTSTableAdapter
             // 
@@ -92,50 +97,85 @@ namespace CLS_Juan_Paredes
             this.tableAdapterManager.TIPUSINCIDENCIATableAdapter = null;
             this.tableAdapterManager.UpdateOrder = CLS_Juan_Paredes.Oracle_dataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             // 
-            // iDPILOTLabel
-            // 
-            iDPILOTLabel.AutoSize = true;
-            iDPILOTLabel.Location = new System.Drawing.Point(36, 109);
-            iDPILOTLabel.Name = "iDPILOTLabel";
-            iDPILOTLabel.Size = new System.Drawing.Size(65, 17);
-            iDPILOTLabel.TabIndex = 3;
-            iDPILOTLabel.Text = "IDPILOT:";
-            // 
             // iDPILOTTextBox
             // 
             this.iDPILOTTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bindingSource1, "IDPILOT", true));
-            this.iDPILOTTextBox.Location = new System.Drawing.Point(119, 104);
+            this.iDPILOTTextBox.Location = new System.Drawing.Point(119, 69);
             this.iDPILOTTextBox.Name = "iDPILOTTextBox";
             this.iDPILOTTextBox.Size = new System.Drawing.Size(100, 22);
             this.iDPILOTTextBox.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 243);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 24);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Incidències pilot.";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(209, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(403, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Punts de sanció";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // frmOracle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(iDPILOTLabel);
+            this.Controls.Add(this.nombres_pilots);
             this.Controls.Add(this.iDPILOTTextBox);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
             this.Name = "frmOracle";
             this.Text = "frmOracle";
             this.Load += new System.EventHandler(this.frmOracle_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.oracle_dataSet1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oracle_dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox nombres_pilots;
         private System.Windows.Forms.BindingSource bindingSource1;
         private Oracle_dataSet oracle_dataSet1;
         private Oracle_dataSetTableAdapters.PILOTSTableAdapter pILOTSTableAdapter;
         private Oracle_dataSetTableAdapters.TableAdapterManager tableAdapterManager;
         private System.Windows.Forms.TextBox iDPILOTTextBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- CLS_Juan_Paredes/frmOracle.cs
+++ CLS_Juan_Paredes/frmOracle.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using Oracle.ManagedDataAccess;
+using Oracle.ManagedDataAccess.Client;
 
 namespace CLS_Juan_Paredes
 {
@@ -24,9 +27,67 @@ namespace CLS_Juan_Paredes
 
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void nombres_pilots_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void nombres_pilots_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String CadenaConnexio = "USER ID = F1; PASSWORD = 1234; DATA SOURCE = 10.0.15.22:1521 / XEPDB1; PERSIST SECURITY INFO = True";
+            OracleConnection cnn = new OracleConnection(CadenaConnexio);
+            OracleCommand command = new OracleCommand();
+            command.Connection = cnn;
+            command.CommandText = "INCIDENCIES_PILOTS.PR_MOSTRARPILOT";
+            command.CommandType = CommandType.StoredProcedure;
+
+            OracleParameter id_pilot = new OracleParameter("id_pilot", OracleDbType.Int32, ParameterDirection.Input);
+            OracleParameter cur_pilot = new OracleParameter("cur_pilot", OracleDbType.RefCursor, ParameterDirection.Output);
+
+            id_pilot.Value = nombres_pilots.SelectedValue;
+
+            command.Parameters.Add(id_pilot);
+            command.Parameters.Add(cur_pilot);
+            cnn.Open();
+            OracleDataAdapter dta = new OracleDataAdapter(command);
+
+            DataSet dts = new DataSet();
+            dta.Fill(dts);
+
+            cnn.Close();
+
+            dataGridView1.DataSource = dts.Tables[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String CadenaConnexio = "USER ID = F1; PASSWORD = 1234; DATA SOURCE = 10.0.15.22:1521 / XEPDB1; PERSIST SECURITY INFO = True";
+            OracleConnection cnn = new OracleConnection(CadenaConnexio);
+            OracleCommand command = new OracleCommand();
+            command.Connection = cnn;
+            command.CommandText = "INCIDENCIES_PILOTS.SUM_PUNTS";
+            command.CommandType = CommandType.StoredProcedure;
+
+            OracleParameter punts_sancio = new OracleParameter("v_total_punts", OracleDbType.Int16, ParameterDirection.ReturnValue);
+            OracleParameter id_pilot = new OracleParameter("id_pilot", OracleDbType.Int16, ParameterDirection.Input);
+
+            id_pilot.Value = nombres_pilots.SelectedValue;
+
+            command.Parameters.Add(punts_sancio);
+            command.Parameters.Add(id_pilot);
+
+            cnn.Open();
+            command.ExecuteScalar();
+
+            string texto = command.Parameters["v_total_punts"].Value.ToString();
+            cnn.Close();
+
+            textBox1.Text = texto;
         }
     }
 }
